ef9cbb0b37758eaadc10182f6f466d23c1060fce
염상윤 <yeomsangyoon@yeomsang-yun-ui-MacBookPro.local>
2022-04-22 18:38:49 +0900

    Day 16

---
+++ 100DaysOfPython#16/main.py
@@ -0,0 +1,20 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+still_remain = True
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print("Your final score is {}/{}".format(quiz.score, quiz.question_number))
