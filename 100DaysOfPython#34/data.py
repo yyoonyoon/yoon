93d5d8b91e6e93ece8ecde0f3643aab2d59fce4a
염상윤 <yeomsangyoon@yeomsang-yun-ui-MacBookPro.local>
2022-06-27 22:50:55 +0900

    Day 34

---
+++ 100DaysOfPython#34/data.py
@@ -0,0 +1,6 @@
+import requests
+
+response = requests.get(url="https://opentdb.com/api.php?amount=10&category=9&type=boolean")
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
